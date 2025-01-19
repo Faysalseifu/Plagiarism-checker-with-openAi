33322ee8bbbe2ace384c86926e176e4abed8156d
Faysalseifu <faysiseifu@gmail.com>
2025-01-19 06:56:42 +0300

    Create main.js

---
+++ Public/Assets/js/main.js
@@ -0,0 +1,23 @@
+/**
+* Template Name: Mentor
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
