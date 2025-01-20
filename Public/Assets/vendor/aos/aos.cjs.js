912dd468c821e444d1954924c8df442f68229eac
Faysalseifu <faysiseifu@gmail.com>
2025-01-20 06:53:36 +0300

    Update aos.cjs.js

--- Public/Assets/vendor/aos/aos.cjs.js
+++ Public/Assets/vendor/aos/aos.cjs.js
@@ -28,3 +28,79 @@ function containsAOSNode(nodes) {
 
   return false;
 }
+
+function check(mutations) {
+  if (!mutations) return;
+
+  mutations.forEach(function (mutation) {
+    var addedNodes = Array.prototype.slice.call(mutation.addedNodes);
+    var removedNodes = Array.prototype.slice.call(mutation.removedNodes);
+    var allNodes = addedNodes.concat(removedNodes);
+
+    if (containsAOSNode(allNodes)) {
+      return callback();
+    }
+  });
+}
+
+function getMutationObserver() {
+  return window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+}
+
+function isSupported() {
+  return !!getMutationObserver();
+}
+
+function ready(selector, fn) {
+  var doc = window.document;
+  var MutationObserver = getMutationObserver();
+
+  var observer = new MutationObserver(check);
+  callback = fn;
+
+  observer.observe(doc.documentElement, {
+    childList: true,
+    subtree: true,
+    removedNodes: true
+  });
+}
+
+var observer = { isSupported: isSupported, ready: ready };
+
+var classCallCheck = function (instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+};
+
+var createClass = function () {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  return function (Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+}();
+
+var _extends = Object.assign || function (target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i];
+
+    for (var key in source) {
+      if (Object.prototype.hasOwnProperty.call(source, key)) {
+        target[key] = source[key];
+      }
+    }
+  }
+
+  return target;
+};
