6cf9af646b10da2bf2d74095d286a49b90ad6677
Faysalseifu <faysiseifu@gmail.com>
2025-01-29 23:19:49 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const { OpenAI } = require('openai');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'views')));
