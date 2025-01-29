565305476455944872665e0f4eb5498c9ef7153e
Faysalseifu <faysiseifu@gmail.com>
2025-01-29 23:25:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'views')));
 
+
+let db;
+
+MongoClient.connect("mongodb+srv://fayselseifu:faysel1234@cluster0.79szisr.mongodb.net/Projectdb", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to MongoDB");
+    db = client.db("Projectdb");
+
+    const qaCollection = db.collection("qa");
+
